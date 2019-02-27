1272e3daceb7e47fd81e3e841b6d7d48cd31172b
Heyson <heyson.severino@endurance.com>
2019-02-27 15:32:30 -0500

    Setup Image-loader and file loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import '../sass/style.scss';
+import Icon from './icon.jpg';
 
 // This is a test function to make sure webpack has compiled assets
 function component() {
     let element = document.createElement('div');
 
     element.innerHTML = 'Brought to you by the Domain.com team.';
+
+       // Add the image to our existing div.
+   var myIcon = new Image();
+   myIcon.src = Icon;
+
+   element.appendChild(myIcon);
 
     return element;
 }
