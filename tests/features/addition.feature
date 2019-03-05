68cfc42a863ff049bf4b64fa475b155bedbf9dc1
malvarado <mario.alvarado@endurance.com>
2019-03-05 13:18:29 -0500

    original-selenium-test-ith-cucumber

--- tests/features/addition.feature
+++ tests/features/addition.feature
@@ -4,18 +4,8 @@ Feature: Addition
 
 Addition is great as a verification exercise to get the Cucumber-js infrastructure up and running
 
-  Scenario: Add two numbers
-    Given the numbers 1 and 3
-    When they are added together
-    Then should the result be 4
-
-  Scenario: Add two numbers
-    Given the numbers 10 and 30
-    When they are added together
-    Then should the result be 40
-
-  Scenario: Search something from bing
-    Given browse to web site "https://www.bing.com"
-    When input keyword "Mars"
-    Then search result should contain "NASA"
+  Scenario: Search something from Brewery Brawl
+    Given browse to web site "http://localhost:3333"
+    When input keyword "angels"
+    Then search result should contain "Angels & Devils"
 
--- tests/steps/browser.js
+++ tests/steps/browser.js
@@ -7,12 +7,11 @@ const {webdriver, Builder,Key, until} = require('selenium-webdriver'),
 selenium = require('selenium-webdriver');
 By = require('selenium-webdriver').By;
 Keys = require('selenium-webdriver').keys;
-chrome = require('selenium-webdriver/chrome')
+chrome = require('selenium-webdriver/chrome');
 
 let driver;
 
 Given(/^browse to web site "([^"]*)"$/, async function(url) {
-    //webdriver = await new Builder().forBrowser('chrome').build();
 
     driver = new selenium.Builder()
         .withCapabilities(selenium.Capabilities.chrome())
@@ -20,14 +19,15 @@ Given(/^browse to web site "([^"]*)"$/, async function(url) {
 
     return driver.get(url);
 
-    //return webdriver.get(url);
 });
 
 When(/^input keyword "([^"]*)"$/, async function (keyword) {
 
-    await driver.findElement({ id: "sb_form_q" }).sendKeys(keyword);
+    await driver.findElement({ name: "query" }).sendKeys(keyword);
 
-    await driver.findElement({ id: "sb_form_go" }).click();
+    await driver.sleep(1000);
+
+    await driver.findElement({ css: ".ghost-button" }).click();
 
     await driver.sleep(2000);
 
@@ -39,11 +39,12 @@ Then(/^search result should contain "([^"]*)"$/, async function (keyword) {
 
     await driver.sleep(2000);
 
-    let result = await driver.findElement({ id: "b_results" }).getText();
+    let result = await driver.findElement({ id: "brewery-results" }).getText();
 
     console.log(result);
 
     driver.quit();
 
     return assert.ok(result.includes(keyword));
+    
 });
