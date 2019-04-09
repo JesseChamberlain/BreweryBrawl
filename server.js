8d0b865b8eb6db396b30400b49287e51c65a766a
Brendan Rielly <brendan.rielly@endurance.com>
2019-04-09 17:22:31 -0400

    Added beer specific pages

--- server.js
+++ server.js
@@ -98,3 +98,37 @@ app.get('/brewery/:id', function(req, res) {
         }
     });
 });
+
+// render our brewery page
+app.get('/beer/:id', function(req, res) {
+    let beerId = req.params.id;
+    let url = `https://api.untappd.com/v4/beer/info/${beerId}?client_id=${
+        process.env.UNTAPPD_CLIENT_ID
+    }&client_secret=${process.env.UNTAPPD_CLIENT_SECRET}`;
+
+    request(url, function(err, response, body) {
+        if (err) {
+            res.render('beer', {
+                beer: null,
+                error: 'Error, please try again'
+            });
+            console.log('error', err);
+        } else {
+            let data = JSON.parse(body);
+            let beer = data.response.beer;
+            if (data.meta.code !== 200) {
+                res.render('beer', {
+                    beer: null,
+                    error: `Error, please try again: ${data.meta.error_detail}`
+                });
+                console.log('error', err);
+            } else {
+                console.log('beer :', beer);
+                res.render('beer', {
+                    beer: beer,
+                    error: null
+                });
+            }
+        }
+    });
+});
