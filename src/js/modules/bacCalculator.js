88e3ce119a9b971a3f0aa452c5e2f6303909019c
Jesse Chamberlain <jesse.chamberlain@gmail.com>
2019-05-03 15:16:56 -0400

    it works

--- src/js/modules/bacCalculator.js
+++ src/js/modules/bacCalculator.js
@@ -1,21 +1,22 @@
 // Module to import images into their corresponding element
 // This is used to allow webpack to compile images in the /index.js file
 export default function bacCalculator() {
-    var bacCalcElement = document.getElementById('bac-calc');
+    let bacCalcElement = document.getElementById('bac-calc');
     if (bacCalcElement) {
-        // var gender = document.querySelector('input[name=gender]:checked');
-        var weightSlider = document.querySelector('input[name=weightSlider]');
-        var drinksSlider = document.querySelector('input[name=drinksSlider]');
-        var hoursSlider = document.querySelector('input[name=hoursSlider]');
-        var weightLabel = document.getElementById('weight-label');
-        var drinksLabel = document.getElementById('drinks-label');
-        var hoursLabel = document.getElementById('hours-label');
-        var bacLabel = document.getElementById('bac-total');
-        var beerABV = document.getElementById('abv').dataset.value;
-        var gender = 'female';
-        var weight = weightSlider.value;
-        var drinks = drinksSlider.value;
-        var hours = hoursSlider.value;
+        // let gender = document.querySelector('input[name=gender]:checked');
+        let weightSlider = document.querySelector('input[name=weightSlider]');
+        let drinksSlider = document.querySelector('input[name=drinksSlider]');
+        let hoursSlider = document.querySelector('input[name=hoursSlider]');
+        let weightLabel = document.getElementById('weight-label');
+        let drinksLabel = document.getElementById('drinks-label');
+        let hoursLabel = document.getElementById('hours-label');
+        let bacLabel = document.getElementById('bac-total');
+        let beerABV = document.getElementById('abv').dataset.value;
+        let gender = 'female';
+        let distRatio = gender === 'male' ? 0.58 : 0.49;
+        let weight = weightSlider.value;
+        let drinks = drinksSlider.value;
+        let hours = hoursSlider.value;
         bacFormula();
 
         weightSlider.addEventListener('change', function() {
@@ -34,21 +35,17 @@ export default function bacCalculator() {
         });
 
         function bacFormula() {
-            var number1 = gender === 'male' ? 0.68 : 0.55;
-            var number2 = gender === 'male' ? 0.015 : 0.017;
             // (A × 5.14/W × r) - .015 × H
             //     Where:
-            //        A: Total alcohol consumed, in ounces (oz)
+            //        A: Total alcohol consumed, in ounces (oz) (drink equal 0.098767oz per 1%)
             //        W: Body weight, in pounds (lbs)
             //        r: The alcohol distribution ratio, 0.73 for man, and 0.66 for women
             //        H: Time passed since drinking, in hours
-
-            var bac =
-                ((drinks * (beerABV * 2.8)) /
-                    (number1 * (weight * 453.592) * (hours / 60))) *
-                    100 -
-                number2;
-            // bac = Math.max(0, Math.round(bac * 100) / 100);
+            let alcoholGrams = drinks * (beerABV * 2.8);
+            let bac =
+                (alcoholGrams / (weight * 453.592 * distRatio)) * 100 -
+                hours * 0.015;
+            bac = Math.round(bac * 100) / 100;
             weightLabel.innerHTML = 'Body Weight: ' + weight;
             drinksLabel.innerHTML = 'Drinks: ' + drinks;
             hoursLabel.innerHTML = 'Hours: ' + hours;
